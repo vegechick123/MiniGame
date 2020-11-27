06a63dc8e01ed02043e7da653f319f46279221cc
vegechick123 <destiny173@163.com>
2020-11-27 20:03:36 +0800

    Light

---
+++ Assets/Scenes/InteractionObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    public bool canTrigger=false;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            canTrigger = true;
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            canTrigger = false;
+    }
+    public void Update()
+    {
+        if (canTrigger && Input.GetKeyDown(KeyCode.V))
+            OnInteraction();
+    }
+    public virtual void OnInteraction()
+    {
+        
+    }
+}
