3e983fb750129a9ad24a48e2904b6c66172df4bf
vegechick123 <destiny173@163.com>
2020-11-28 14:42:03 +0800

    Gear Refractor

--- Assets/Script/Gear.cs
+++ Assets/Script/Gear.cs
@@ -4,64 +4,50 @@ using UnityEngine;
 
 public class Gear : MonoBehaviour
 {
-    public Vector3[] position;
-    public float[] rotation;    
-    public int curIndex=0;
-    public int curIndex2 = 0;
-    public int curIndex3 = 0;
+    //public bool fixCenterOfMass;
+    public float rotateSpeed;
+    public float moveSpeed;
+    public float externForce;
+    public float deltaAngle { get { return angle - m_rigidbody2D.rotation; } }//与目标角度的差值
+    public float rotateAngle { get { return Mathf.Min(Time.fixedDeltaTime * rotateSpeed, Mathf.Abs(deltaAngle)); } }//当前单位时间内旋转的角度
+    Rigidbody2D m_rigidbody2D;
+
+    public float[] verticle;
+    public float[] horizon;
+    public float[] rotation;
+    public int curIndexVertical = 0;
+    public int curIndexHorizon = 0;
+    public int curIndexRotation = 0;
     public int max = 10;
-  
-    void Refresh(int targetIndex)
-    {
-        if (0 <= targetIndex && targetIndex < position.Length)
-        {
-            transform.localPosition= position[targetIndex];
-            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, rotation[targetIndex]));
-            curIndex = targetIndex;
-        }
-    }
-    public void ChangeIndex(int chageValue)
-    {
-        int targetIndex = Mathf.Clamp(curIndex + chageValue,0, position.Length);
-        if(targetIndex!=curIndex)
-        {
-            Refresh(targetIndex);
-        }
-    }
-   void MoveVer(int targetIndex)
-    {
-        Vector3 pos = transform.position;
-        pos.y += targetIndex;
-        
-            transform.position = pos;          
-            curIndex2 = targetIndex;
-        
-       
-    }
-    public void ChangeIndex2(int chageValue)
-    {
-        int targetIndex = Mathf.Clamp(curIndex2 + chageValue, 0, max);
-        if (targetIndex != curIndex2)
-        {
-            MoveVer(targetIndex);
-        }
-    }
-    void MoveHor(int targetIndex)
-    {
-        Vector3 pos = transform.position;
-        pos.x += targetIndex;
+    public float angle { get { return rotation[curIndexRotation]; } }
+    public Vector2 targetPosition { get { 
+            return  new Vector2(horizon.Length<=0?m_rigidbody2D.position.x:horizon[curIndexHorizon],
+                verticle.Length <= 0 ? m_rigidbody2D.position.y : verticle[curIndexVertical]); } }
 
-        transform.position = pos;
-        curIndex3 = targetIndex;
-
-
-    }
-    public void ChangeIndex3(int chageValue)
+    private void Awake()
     {
-        int targetIndex = Mathf.Clamp(curIndex3 + chageValue, 0, max);
-        if (targetIndex != curIndex3)
-        {
-            MoveHor(targetIndex);
-        }
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    public void ChangeHorizon(int chageValue)
+    {
+        int targetIndex = Mathf.Clamp(curIndexVertical + chageValue, 0, horizon.Length-1);
+        curIndexVertical = targetIndex;
+    }
+    public void ChangeVerticle(int chageValue)
+    {
+        int targetIndex = Mathf.Clamp(curIndexVertical + chageValue, 0, verticle.Length-1);
+        curIndexVertical = targetIndex;
+    }
+    public void ChangeRotation(int chageValue)
+    {
+        int targetIndex = Mathf.Clamp(curIndexRotation + chageValue, 0, rotation.Length-1);
+        curIndexRotation = targetIndex;
+    }
+    private void FixedUpdate()
+    {
+        float curAngle = Mathf.Sign(deltaAngle) * rotateAngle + m_rigidbody2D.rotation;
+        Vector2 curPosition = Vector2.MoveTowards(m_rigidbody2D.position, targetPosition,moveSpeed*Time.fixedDeltaTime);
+        m_rigidbody2D.MoveRotation(curAngle);
+        m_rigidbody2D.MovePosition(curPosition);
     }
 }
--- Assets/Script/InteractionObject/Machine.cs
+++ Assets/Script/InteractionObject/Machine.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum MoveType
+{
+    Horizon,
+    Verticle,
+    Rotation
+}
 public class Machine : InteractionObject
 {
     public Gear targetGear;
+    public MoveType moveType;
     public int chageIndex = 1;
     public override void OnInteraction(KeyCode inputKey)
     {
         base.OnInteraction(inputKey);
-        targetGear.ChangeIndex(chageIndex);
+        switch (moveType)
+        {
+            case MoveType.Horizon:
+                targetGear.ChangeHorizon(chageIndex);
+                break;
+            case MoveType.Verticle:
+                targetGear.ChangeVerticle(chageIndex);
+                break;
+            case MoveType.Rotation:
+                targetGear.ChangeRotation(chageIndex);
+                break;
+                
+        }
     }
 }
