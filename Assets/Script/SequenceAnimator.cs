c471514199e8f2761bbb26020b56e9136421aaca
alastor-lan <71885079+alastor-lan@users.noreply.github.com>
2020-11-29 11:59:58 +0800

    text

---
+++ Assets/Script/SequenceAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceAnimator : MonoBehaviour
+{
+    public float WaitBetween = 0.15f;
+    public float WaitEnd = 0.5f;
+
+    List<Animator> _animators;
+    void Start()
+    {
+        _animators = new List<Animator>(GetComponentsInChildren<Animator>());
+        StartCoroutine(DoAnimation());
+    }
+    IEnumerator DoAnimation()
+    {
+        while (true)
+        {
+            foreach(var animator in _animators)
+            {
+                animator.SetTrigger("DoSlidingUp");
+                yield return new WaitForSeconds(WaitBetween);
+
+            }
+            yield return new WaitForSeconds(WaitEnd);
+        }
+    }
+  
+}
