44918a15afe56a442e56f39e25d4ac7305bcdd2c
Muhlik <56724748+ifyoumaytryit@users.noreply.github.com>
2020-11-29 14:39:53 +0800

    particel_effects

--- Assets/Script/InteractionObject/Obstacle.cs
+++ Assets/Script/InteractionObject/Obstacle.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Obstacle : InteractionObject
 {
+    public GameObject breakParticle;
     //Collider2D colider; 
     public override void OnInteraction(KeyCode inputKey)
     {
         base.OnInteraction(inputKey);
+        Instantiate(breakParticle, gameObject.transform.position, breakParticle.transform.rotation);
         gameObject.SetActive(false);
+ 
     }
 
 }
